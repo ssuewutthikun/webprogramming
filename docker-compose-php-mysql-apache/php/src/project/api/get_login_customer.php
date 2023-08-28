ba7a8b31e42d8677b385189e5bb87b2c7a4595bc
ssuewutthikun <31299899+ssuewutthikun@users.noreply.github.com>
2023-08-28 13:27:29 +0700

    Add files via upload

---
+++ docker-compose-php-mysql-apache/php/src/project/api/get_login_customer.php
@@ -0,0 +1,20 @@
+<?php
+    #header
+    @header('Content-Type: application/json');
+    @header("Access-Control-Allow-Origin: *");
+    @header('Access-Control-Allow-Headers: X-Requested-With, content-type, access-control-allow-origin, access-control-allow-methods, access-control-allow-headers'); 
+?>   
+<?php
+    #connection and data
+    require ("../config/config_db.php");
+    print_r($conn);
+?>
+<?php
+    #process
+?>
+<?php
+    #output
+?>
+<?php
+    #log
+?>
