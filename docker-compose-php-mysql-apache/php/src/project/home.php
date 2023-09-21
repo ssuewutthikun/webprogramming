d9e455602ec025198ca343297da8840c04873362
ssuewutthikun <31299899+ssuewutthikun@users.noreply.github.com>
2023-09-21 14:00:42 +0700

    Add files via upload

--- docker-compose-php-mysql-apache/php/src/project/home.php
+++ docker-compose-php-mysql-apache/php/src/project/home.php
@@ -2,22 +2,26 @@
     ob_start();
     session_start();
     require "config/config_db.php";
-    $strSQL="SELECT id,name FROM customer WHERE session ='".session_id()."' ";
+    $strSQL="SELECT id,name FROM customer WHERE session = '".session_id()."' ";
     $query = @mysqli_query($conn,$strSQL);
     $resultQuery = @mysqli_fetch_array($query);
-    if(@$resultQuery['id']!=""){
+    print_r($resultQuery);
+    if($resultQuery['id']!=""){
         //print_r("show form");
     }else{
-        //print_r("go to login");
+        //print_r("go to login.php");
         @mysqli_close($conn);
-        header("location: http://localhost:8080/project/login.php");
+        header("location: http://127.0.0.1:8080/project/login.php");
         exit;
     }
-    ob_end_flush();
+    ob_end_clean();
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="./css/main_style.css">
 </head>
 <body>
@@ -35,12 +39,105 @@
             </ul>
         </nav>
         <div class="column content" style="background-color: burlywood;">
-            content
+        <div id="content">
+                <div class="row" style="padding:10px">
+                    <div class="col-md-3 mb-3">
+                        <div class="card" style="background-color: #FFF;margin:5px">
+                            <div class="card-image" style="width:150px;height:150px;align-items: center;">
+                                <img src="./assets/images/product-default.png" class="card-img-top" />
+                            </div>
+                            <div class="card-content">
+                                <h5 class="card-title">ชื่อสินค้า 1</h5>
+                                <p class="card-text">รายละเอียดสินค้า 1</p>
+                                <p class="card-text">ราคา: ราคาสินค้า 1</p>
+                                <button  class="btn btn-danger">เพิ่มลงตะกร้า</button>                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 mb-3">
+                        <div class="card" style="background-color: #FFF;margin:5px">
+                            <div class="card-image" style="width:150px;height:150px;align-items: center;">
+                                <img src="./assets/images/product-default.png" class="card-img-top" />
+                            </div>
+                            <div class="card-content">
+                                <h5 class="card-title">ชื่อสินค้า 2</h5>
+                                <p class="card-text">รายละเอียดสินค้า 2</p>
+                                <p class="card-text">ราคา: ราคาสินค้า 2</p>
+                                <button  class="btn btn-danger">เพิ่มลงตะกร้า</button>                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 mb-3">
+                        <div class="card" style="background-color: #FFF;margin:5px">
+                            <div class="card-image" style="width:150px;height:150px;align-items: center;">
+                                <img src="./assets/images/product-default.png" class="card-img-top" />
+                            </div>
+                            <div class="card-content">
+                                <h5 class="card-title">ชื่อสินค้า 3</h5>
+                                <p class="card-text">รายละเอียดสินค้า 3</p>
+                                <p class="card-text">ราคา: ราคาสินค้า 3</p>
+                                <button  class="btn btn-danger">เพิ่มลงตะกร้า</button>                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 mb-3">
+                        <div class="card" style="background-color: #FFF;margin:5px">
+                            <div class="card-image" style="width:150px;height:150px;align-items: center;">
+                                <img src="./assets/images/product-default.png" class="card-img-top" />
+                            </div>
+                            <div class="card-content">
+                                <h5 class="card-title">ชื่อสินค้า 4</h5>
+                                <p class="card-text">รายละเอียดสินค้า 4</p>
+                                <p class="card-text">ราคา: ราคาสินค้า 4</p>
+                                <button  class="btn btn-danger">เพิ่มลงตะกร้า</button>                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 mb-3">
+                        <div class="card" style="background-color: #FFF;margin:5px">
+                            <div class="card-image" style="width:150px;height:150px;align-items: center;">
+                                <img src="./assets/images/product-default.png" class="card-img-top" />
+                            </div>
+                            <div class="card-content">
+                                <h5 class="card-title">ชื่อสินค้า 5</h5>
+                                <p class="card-text">รายละเอียดสินค้า 5</p>
+                                <p class="card-text">ราคา: ราคาสินค้า 5</p>
+                                <button  class="btn btn-danger">เพิ่มลงตะกร้า</button>                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 mb-3">
+                        <div class="card" style="background-color: #FFF;margin:5px">
+                            <div class="card-image" style="width:150px;height:150px;align-items: center;">
+                                <img src="./assets/images/product-default.png" class="card-img-top" />
+                            </div>
+                            <div class="card-content">
+                                <h5 class="card-title">ชื่อสินค้า 6</h5>
+                                <p class="card-text">รายละเอียดสินค้า 6</p>
+                                <p class="card-text">ราคา: ราคาสินค้า 6</p>
+                                <button  class="btn btn-danger">เพิ่มลงตะกร้า</button>                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 mb-3">
+                        <div class="card" style="background-color: #FFF;margin:5px">
+                            <div class="card-image" style="width:150px;height:150px;align-items: center;">
+                                <img src="./assets/images/product-default.png" class="card-img-top" />
+                            </div>
+                            <div class="card-content">
+                                <h5 class="card-title">ชื่อสินค้า 7</h5>
+                                <p class="card-text">รายละเอียดสินค้า 7</p>
+                                <p class="card-text">ราคา: ราคาสินค้า 7</p>
+                                <button  class="btn btn-danger">เพิ่มลงตะกร้า</button>                
+                            </div>
+                        </div>
+                    </div>
+                    <!--- เพิ่ม card สินค้า --->
         </div>
     </div>
 
-    <footer style="background-color: pink;">
+    <!--<footer style="background-color: pink;">
         <p>Footer</p>
-    </footer>
+    </footer>-->
 </body>
 </html>
