5f97e3b4dab457b16150b4a1b6584019aa3e196a
ssuewutthikun <31299899+ssuewutthikun@users.noreply.github.com>
2023-10-12 10:35:10 +0700

    Add files via upload

---
+++ docker-compose-php-mysql-apache/php/src/project/api/set_cart_add.php
@@ -0,0 +1,40 @@
+<?php ob_start(); ?>
+<?php
+    #header
+    @header('Content-Type: application/json');
+    @header("Access-Control-Allow-Origin: *");
+    @header('Access-Control-Allow-Headers: X-Requested-With, content-type, access-control-allow-origin, access-control-allow-methods, access-control-allow-headers');
+?>   
+<?php
+    #connection and data include  OR require
+    require ("../config/config_db.php");
+?>
+<?php
+    #input
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $content = @file_get_contents('php://input');
+        $json_data = @json_decode($content, true);
+        $customerId = trim($json_data["customer_id"]);
+        $inputProductId = trim($json_data["product_id"]);
+        $inputNum = trim($json_data["num"]);
+        $inputPrice = trim($json_data["price"]);
+        $total = $inputNum*$inputPrice;
+        $sku=null;
+    }else{
+        ob_end_clean();
+        @header("HTTP/1.0 412 Precondition Failed");
+        die();         
+    }
+?>
+<?php
+    #process
+    $strSQL="INSERT INTO  cart (customer_id,product_id,sku,num,price,total) VALUE ('".$customerId."','".$inputProductId."','".$sku."','".$inputNum."','".$inputPrice."','".$total."')";
+    //print_r($strSQL);
+    $query = @mysqli_query($conn,$strSQL);
+?>
+<?php
+    #output
+?>
+<?php
+    #log function
+?>
